067f69133e6c96ebcff295ced584d707c99b72fb
tuanhust2111 <84861029+tuanhust2111@users.noreply.github.com>
2021-08-21 12:17:07 +0700

    hihi js.s
    no

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text1: "He lu Lanh cute , a Tuấn đây!",
+  text2: "Anh có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
+  text3: "Em quý anh có phải không nào ._.",
+  text4: "Nếu em ko trả lời mà thoát ra tức là muốn làm ny anh rùi đó nha :v",
+  text5: "Em mơ à???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text7: "lí do em quý anh đi :vvvv",
+  text8: "Gửi cho anh <3",
+  text9: "Vì anh đẹp try vlllll",
+  text10: "Anh biết mà ^^ Love you 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Khi nào có dịp vô trong đó anh sẽ chở em đi chơi , còn giờ thì inbox với anh nha",
   text12: "Okii lunn <3",
 };
 
@@ -35,7 +35,7 @@ $(document).ready(function () {
     Swal.fire({
       title: textConfig.text1,
       text: textConfig.text2,
-      imageUrl: "img/cuteCat.jpg",
+      imageUrl: "img/lananhcute",
       imageWidth: 300,
       imageHeight: 300,
       background: '#fff url("img/iput-bg.jpg")',
